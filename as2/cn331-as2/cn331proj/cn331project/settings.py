a4b913608ecd66fcf2cd9a1650692e1ac7f4ddc1
6710525020 <6710525020@student.tu.ac.th>
2025-09-20 23:17:35 +0700

    Update settings.py

--- as2/cn331-as2/cn331proj/cn331project/settings.py
+++ as2/cn331-as2/cn331proj/cn331project/settings.py
@@ -131,9 +131,6 @@ LOGOUT_REDIRECT_URL = '/'
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
-
 import os
 import dj_database_url
 
@@ -166,6 +163,7 @@ else:
     }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 
 
