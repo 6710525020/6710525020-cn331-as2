6e4ca5c8d114658bb0cd09aa2d527187cf2d65e1
6710525020 <6710525020@student.tu.ac.th>
2025-09-20 19:43:53 +0700

    Update build.sh

--- as2/cn331-as2/cn331proj/build.sh
+++ as2/cn331-as2/cn331proj/build.sh
@@ -8,4 +8,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --username punchadmin --email "sarasine3584@gmail.com" --noinput
+python manage.py createsuperuser --username admin1 --email "sarasine3584@gmail.com" --noinput
