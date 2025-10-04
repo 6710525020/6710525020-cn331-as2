d896f1954d32e072a44ab5a86900d788a96e207c
6710525020 <6710525020@student.tu.ac.th>
2025-10-04 23:27:15 +0700

    Add files via upload

--- cn331-as2/cn331proj/booking/tests.py
+++ cn331-as2/cn331proj/booking/tests.py
@@ -4,6 +4,7 @@ from .models import Room,Booking
 from django.utils import timezone
 from datetime import timedelta
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
 
@@ -38,8 +39,8 @@ class bookingTest(TestCase):
 
     def test_end_before_start(self):
         # check that start time is before end time
-        start_time = timezone.now() - timedelta(hours=1)
-        end_time = start_time + timedelta(hours=1)
+        start_time = timezone.now() + timedelta(hours=2)
+        end_time = start_time - timedelta(hours=1)
         booking= Booking(room=self.room,user=self.user2,start_time=start_time,end_time=end_time)
         with self.assertRaisesMessage(Exception,"End time must be after start time."):
             booking.clean()
@@ -53,7 +54,7 @@ class bookingTest(TestCase):
         overlap_start = start_time + timedelta(minutes=30)
         overlap_end = end_time + timedelta(minutes=30)
 
-        with self.assertRaisesMessage(Exception, "Overlaps with another booking"):
+        with self.assertRaisesMessage(Exception, "This time slot overlaps with another booking."):
             Booking.objects.create(room=self.room,user=self.user1,start_time=overlap_start,end_time=overlap_end)
     
     def test_cannot_exceed_max_hours(self):
@@ -75,10 +76,73 @@ class bookingTest(TestCase):
             booking.clean()
 
     def test_booking_per_user_per_room(self):
-        # one user can only booking one time per room\
+        # one user can only booking one time per room
         now = timezone.now()
-        Booking(room=self.room,user=self.user1,start_time=now + timedelta(minutes=10),end_time=now + timedelta(minutes=1))
-        booking2 = Booking(room=self.room,user=self.user1,start_time=now + timedelta(hours=1, minutes=10),end_time=now + timedelta(hours=2))
+        booking1 = Booking(room=self.room,user=self.user1,start_time= now + timedelta(minutes=10),end_time= now + timedelta(minutes=50))
 
-        with self.assertRaisesMessage(Exception, "You already have an active or upcoming booking for this room."):
+        booking1.full_clean()
+        booking1.save()
+
+        booking2 = Booking(room=self.room,user=self.user1,start_time=now + timedelta(hours=1, minutes=10),end_time=now + timedelta(hours=2))
+        with self.assertRaisesMessage(ValidationError, "You already have an active or upcoming booking for this room."):
             booking2.save()
+
+    def test_room_required(self):
+        # must have room id
+        start_time = timezone.now() + timedelta(hours=1)
+        end_time = start_time + timedelta(hours=1)
+        booking = Booking(user=self.user2,start_time=start_time,end_time=end_time)
+        with self.assertRaisesMessage(Exception, "Room is required for booking"):
+            booking.clean()
+
+    def test_user_required(self):
+        # must have user id
+        start_time = timezone.now() + timedelta(hours=1)
+        end_time = start_time + timedelta(hours=1)
+        booking = Booking(room=self.room,start_time=start_time,end_time=end_time)
+        with self.assertRaisesMessage(Exception, "User is required for booking"):
+            booking.clean()
+
+    def test_index_status_code(self):
+        # test first page
+        c = Client()
+        resp = c.get(reverse("index"))
+        self.assertEqual(resp.status_code,200)
+        self.assertIn("room", resp.context)
+        self.assertGreaterEqual(resp.context["room"].count(),1)    
+
+    def test_room_detail_invalid(self):
+        # test room detail
+        c = Client()
+        resp = c.get(reverse("room_detail", args=[999999]))
+        self.assertEqual(resp.status_code, 404)
+
+    def test_mine_booking(self):
+        # show all user booking
+        c = Client()
+        start_time = timezone.now() + timedelta(hours=1)
+        end_time = start_time + timedelta(hours=1)
+        Booking(room=self.room,user=self.user1,start_time=start_time,end_time=end_time)
+        Booking(room=self.room,user=self.user2,start_time=start_time,end_time=end_time)
+
+        c.login(username="jigsaw", password="123456")
+        resp = c.get(reverse("mine"))
+        self.assertEqual(resp.status_code, 200)
+        bookings = resp.context.get("bookings")
+        self.assertTrue(all(b.user == self.user1 for b in bookings))
+
+    def test_cancle_booking_owner_only(self):
+        # owner can cancle booking
+        c = Client()
+        start_time = timezone.now() + timedelta(hours=1)
+        end_time = start_time + timedelta(hours=1)
+        booking = Booking(room=self.room,user=self.user1,start_time=start_time,end_time=end_time)
+        c.login(username="jigsaw", password="123456")
+        resp = c.post(reverse("cancle_booking", args=[booking.id]))
+        self.assertEqual(resp.status_code, 302)
+        self.assertFalse(Booking.objects.filter(id = booking.id).exists())
+
+        # other try to cancle booking
+        booking2 = Booking(room=self.room,user=self.user2,start_time=start_time,end_time=end_time)
+        resp2 = c.post(reverse("cancle_booking", args=[booking2.id]))
+        self.assertEqual(resp2.status_code, 404)
